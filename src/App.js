89c6f13b99289beb4316a1957a42e58d414cd4eb
rashedul alam <rashedulalam362@gmail.com>
2021-10-07 17:40:26 +0600

    material project on going

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { makeStyles } from '@mui/styles';
+import Navbar from './components/Navbar';
+import Cardinfo from './components/Cardinfo';
 
+
+
+const useStyles = makeStyles({
+  
+});
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const classes = useStyles();
+    return <div>
+      <Navbar/>
+      <Cardinfo />
+      <Cardinfo />
+   
     </div>
-  );
+    
 }
 
 export default App;
---
+++ src/components/Cardinfo.jsx
@@ -0,0 +1,47 @@
+
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginBottom: theme.spacing(5),
+  },
+  media: {
+    height: 250,
+    [theme.breakpoints.down("sm")]: {
+      height: 150,
+    },
+  },
+}));
+
+function Cardinfo({ img, title }) {
+  const classes=useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={img} title="My Post" />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {title}
+          </Typography>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            consectetur earum est.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Cardinfo
